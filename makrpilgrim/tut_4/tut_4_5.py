989aa3c3890b02fece975aa9b9c1448c878b34da
Dusan Krajcovic <dusan.krajcovic@gmail.com>
2016-06-17 15:02:37 +0200

    mark pigrim tut 4 (strings)

--- makrpilgrim/tut_4/tut_4_5.py
+++ makrpilgrim/tut_4/tut_4_5.py
@@ -4,7 +4,19 @@ a_string = 'My alphabet starts where your alphabet ends.'
 print(a_string[3:11])
 
 by = b'abcd\x65'
-by += b'\xff'
+# by += b'\xff'
 print(by, len(by), type(by))
 # by[0] = 102
+
+barr = bytearray(by)
+print(barr, len(barr), type(barr))
+barr[0] = 102
+print(barr, len(barr), type(barr))
+de = by.decode('ascii')
+print(de, len(de), type(de))
+
+a_string = '深入 Python'
+print(len(a_string))
+by = a_string.encode('utf-8')
+print(by, type(by))
 
